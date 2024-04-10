a40b10d466f2bec44becfb04405b60de0e3d933f
Tomasz Mach <m4chtomasz@gmail.com>
2024-04-10 20:01:58 +0200

    Add: First Vite APP

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="color">La mia prima app con Vite!</div>
+</template>
+
+<style scoped>
+.color {
+    color: red;
+    font-size: 100px;
+}
+</style>
